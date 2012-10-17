bc8836dca3205ff8e8cde2956960783e2cebf741
Benedikt Bergenthal <benedikt@kdrennert.de>
2012-10-17 17:12:50 +0200

    mapped blue

--- lib/HWheel.cpp
+++ lib/HWheel.cpp
@@ -18,22 +18,29 @@ HWheel::HWheel(float seed)
 		auto c = std::make_shared<HColor>();
 		float red,blue,yellow;
 		float n = pos[i];
-		if (n <= 120) {
+		if (n <= 120 && n >= 0) {
 			yellow = 100 -(100/120 * n);
-		} else if (n > 240) {
+		} else if (n >= 240 && n <=360) {
 			yellow = 100 - (n - 240) * 100/120;
 		} else {
 			yellow = 0;
 		}
 
-		if (n <= 120) {
+		if (n <= 120 && n >= 0) {
 			red = (100/120 * n);
-		} else if (n < 240) {
+		} else if (n < 240 && n >120) {
 			red = 100 - (100/120 *(n-120));
 		} else {
 			red = 0;
 		}
-		
+
+		if (n >= 120 && n <= 240) {
+			blue = 120 - (100/120 * n);
+		} else if (n >=240 && n <=360) {
+			blue = 100 - (100/120 * (n-240));
+		} else {
+			blue = 0;
+		}
 	}	
 }
 
